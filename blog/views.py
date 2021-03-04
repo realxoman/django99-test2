138b8ad6d4ad01a6ffa85bc193fa30a40c5eb30a
Alixo <fromiran98@gmail.com>
2021-03-04 18:49:59 +0330

    1.0.87

--- blog/views.py
+++ blog/views.py
@@ -103,5 +103,11 @@ def addproduct(request):
 seller, created = Group.objects.get_or_create(name='seller')
 
 
+register = template.Library()
+
+@register.filter(name='has_group')
+def has_group(user, group_name):
+    group = Group.objects.get(name=group_name)
+    return True if group in user.groups.all() else False
 
 
