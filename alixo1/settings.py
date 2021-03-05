96a8163bc14bfaafa782718a68c4dd2786d2b927
Alixo <fromiran98@gmail.com>
2021-03-05 16:29:26 +0330

    1.1.20

--- alixo1/settings.py
+++ alixo1/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'alixo1.urls'
--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,6 @@ from django.conf import settings
 from django.contrib.auth.models import User
 
 
-
 # Create your models here.
 
 
